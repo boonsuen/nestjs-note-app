c8a8173306a820b38dcdac69489079fa9386bfd7
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-21 11:02:00 +0800

    Inject Notes Service into the Controller

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { NotesService } from './notes.service';
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+  constructor(private notesService: NotesService) {}
+}
