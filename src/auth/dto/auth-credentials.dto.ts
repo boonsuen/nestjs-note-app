017a9872c3ec1f5f76a34d02de6592643fee331c
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-21 16:39:14 +0800

    Validation for auth credentials

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,20 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
+  // Passwords will contain at least 1 upper case letter
+  // Passwords will contain at least 1 lower case letter
+  // Passwords will contain at least 1 number or special character
+  // There is no length validation (min, max) in this regex!
   password: string;
 }
