b87ec715cecad8f8ddbec80e6677417b55caa024
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-24 00:21:41 +0800

    Configuration for deployment

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  await app.listen(8080, '0.0.0.0');
 }
 bootstrap();
