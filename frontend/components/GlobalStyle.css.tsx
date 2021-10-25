8346ff24d899111b9350ebb75c7e02f018ab907d
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-25 22:45:21 +0800

    Add nprogress

--- frontend/components/GlobalStyle.css.tsx
+++ frontend/components/GlobalStyle.css.tsx
@@ -2,14 +2,87 @@ import { createGlobalStyle, css } from 'styled-components';
 
 export const fontFaceRules = `
   @font-face {
-    font-family: "Inter";
-    src: url("/fonts/Inter.var.woff2") format("woff2 supports variations"),
-         url("/fonts/Inter.var.woff2") format("woff2-variations");
+    font-family: 'Inter';
+    src: url('/fonts/Inter.var.woff2') format('woff2 supports variations'),
+      url('/fonts/Inter.var.woff2') format('woff2-variations');
     font-weight: 100 900;
   }
 `;
 
+const nProgressStyle = css`
+  /* Make clicks pass-through */
+  #nprogress {
+    pointer-events: none;
+  }
+
+  #nprogress .bar {
+    background: #1890ff;
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 1px;
+  }
+
+  /* Fancy blur effect */
+  #nprogress .peg {
+    display: block;
+    position: absolute;
+    right: 0px;
+    width: 100px;
+    height: 100%;
+    box-shadow: 0 0 10px red, 0 0 5px red;
+    opacity: 1;
+    transform: rotate(3deg) translate(0px, -4px);
+  }
+
+  /* Remove these to get rid of the spinner */
+  #nprogress .spinner {
+    display: block;
+    position: fixed;
+    z-index: 1031;
+    top: 15px;
+    right: 15px;
+  }
+
+  #nprogress .spinner-icon {
+    width: 18px;
+    height: 18px;
+    box-sizing: border-box;
+
+    border: solid 2px transparent;
+    border-top-color: #1890ff;
+    border-left-color: #1890ff;
+    border-radius: 50%;
+
+    animation: nprogress-spinner 400ms linear infinite;
+  }
+
+  .nprogress-custom-parent {
+    overflow: hidden;
+    position: relative;
+  }
+
+  .nprogress-custom-parent #nprogress .spinner,
+  .nprogress-custom-parent #nprogress .bar {
+    position: absolute;
+  }
+
+  @keyframes nprogress-spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 const GlobalStyle = createGlobalStyle`
+  ${props => nProgressStyle}
+
   * {
     box-sizing: border-box;
   }
@@ -65,12 +138,12 @@ const sizes = {
   '1275': 1275,
   '1150': 1150,
   '1050': 1050,
-  '600': 600
+  '600': 600,
 };
 
 export const media = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args: any[]) => css`
-    @media (max-width: ${sizes[label] / 16}em) {      
+    @media (max-width: ${sizes[label] / 16}em) {
       ${css.call(undefined, ...args)}
     }
   `;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,14 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import GlobalStyle, { fontFaceRules } from '../components/GlobalStyle.css';
 import 'antd/dist/antd.css';
+
+// NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
