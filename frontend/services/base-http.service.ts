e306cafed9c7670c9431ad7cf80dbb2a1efa6756
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-25 18:33:40 +0800

    Update BaseHttpService to use axios instance

--- frontend/services/base-http.service.ts
+++ frontend/services/base-http.service.ts
@@ -1,36 +1,38 @@
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import Router from 'next/router';
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080'
+});
 
 export default class BaseHttpService {
-  API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
-  _accessToken: string | null = null;
-  router = useRouter();
+  API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
 
   async get(endpoint: string, options = {}) {
-    Object.assign(options, this._getCommonOptions());
+    // Object.assign(options, this._getCommonOptions());
     return axios
-      .get(`${this.API_BASE_URL}/${endpoint}`, options)
+      .get(endpoint, options)
       .catch((error) => this._handleHttpError(error));
   }
 
-  async post(endpoint: string, data = {}, options = {}) {
-    Object.assign(options, this._getCommonOptions());
-    return axios
-      .post(`${this.API_BASE_URL}/${endpoint}`, data, options)
+  async post(endpoint: string, data = {}, options: AxiosRequestConfig<{}> = {}) {
+    return instance
+      .post(endpoint, data, options)
       .catch((error) => this._handleHttpError(error));
   }
 
   async delete(endpoint: string, options = {}) {
-    Object.assign(options, this._getCommonOptions());
+    // Object.assign(options, this._getCommonOptions());
     return axios
-      .delete(`${this.API_BASE_URL}/${endpoint}`, options)
+      .delete(endpoint, options)
       .catch((error) => this._handleHttpError(error));
   }
 
   async patch(endpoint: string, data = {}, options = {}) {
-    Object.assign(options, this._getCommonOptions());
+    // Object.assign(options, this._getCommonOptions());
     return axios
-      .patch(`${this.API_BASE_URL}/${endpoint}`, data, options)
+      .patch(endpoint, data, options)
       .catch((error) => this._handleHttpError(error));
   }
 
@@ -45,35 +47,6 @@ export default class BaseHttpService {
   }
 
   _handle401() {
-    window.location.hash = '/signin';
-  }
-
-  _getCommonOptions() {
-    const token = this.loadToken();
-
-    return {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  }
-
-  get accessToken() {
-    return this._accessToken ? this._accessToken : this.loadToken();
-  }
-
-  saveToken(accessToken: string): void {
-    this._accessToken = accessToken;
-    return localStorage.setItem('accessToken', accessToken);
-  }
-
-  loadToken() {
-    const token = localStorage.getItem('accessToken');
-    this._accessToken = token;
-    return token;
-  }
-
-  removeToken() {
-    localStorage.removeItem('accessToken');
+    Router.push('/login');
   }
 }
