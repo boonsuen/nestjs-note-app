26d0ea14c81d9fd07f36968f8eb99f2456580209
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-26 00:44:15 +0800

    Make body optional

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,9 +1,9 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateNoteDto {
   @IsNotEmpty()
   title: string;
 
-  @IsNotEmpty()
+  @IsOptional()
   body: string;
 }
