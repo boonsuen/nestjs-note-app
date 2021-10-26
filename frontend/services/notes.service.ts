9a1de40a9037a89c0b77ad42c429a5d77e100bb0
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-26 13:56:04 +0800

    updateNote service

--- frontend/services/notes.service.ts
+++ frontend/services/notes.service.ts
@@ -3,9 +3,25 @@ import { Note } from '../types/note.type';
 import BaseHttpService from './base-http.service';
 
 export default class NotesService extends BaseHttpService {
-  async createNote(title: string, body: string = ""): Promise<Note> {
+  async createNote(title: string, body: string = ''): Promise<Note> {
     const result = await axios.post<Note>(
       `${this.API_BASE_URL}/notes`,
+      {
+        title,
+        body,
+      },
+      { withCredentials: true },
+    );
+    return result.data;
+  }
+
+  async updateNote(
+    id: string,
+    title: string,
+    body: string = '',
+  ): Promise<Note> {
+    const result = await axios.post<Note>(
+      `${this.API_BASE_URL}/notes/${id}`,
       {
         title,
         body,
