64b333469fbfb3105000fe333d637615608541c2
Boonsuen Oh <boonsuenoh@gmail.com>
2021-10-26 14:41:31 +0800

    Make UpdateNoteDto body optional

--- src/notes/dto/update-note.dto.ts
+++ src/notes/dto/update-note.dto.ts
@@ -1,9 +1,9 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateNoteDto {
   @IsNotEmpty()
   title: string;
 
-  @IsNotEmpty()
+  @IsOptional()
   body: string;
 }
